40a7013d38df09fab77c0319a66260e0f8f562ab
lpault29@gmail.com Ly <lpault29@gmail.com>
2018-05-05 21:49:46 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.size == 1 
+    array.join
+  elsif array.size == 2 
+    array.join(" and ") 
+  elsif array.size >= 3 
+    together = array.join(", ") 
+  puts together
+  last = array.pop
+  together.sub(last,"and #{last}")
+  else 
+    puts "error"
+  end 
 end
